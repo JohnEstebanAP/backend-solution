0becdd5e63446c871194e28fce8542596ca38ac1
John Esteban Alvarez Piedrahita <esteban.ea145@gmail.com>
2022-07-17 14:58:11 -0500

    se agregaron los mappers, para mapiar las entidades DTO

--- src/main/java/co/com/sofka/questions/mapper/AnsmerMapper.java
+++ src/main/java/co/com/sofka/questions/mapper/AnsmerMapper.java
@@ -1,4 +1,32 @@
 package co.com.sofka.questions.mapper;
 
+import co.com.sofka.questions.collections.Answer;
+import co.com.sofka.questions.model.AnswerDTO;
+import org.springframework.stereotype.Component;
+
+import java.util.function.Function;
+
+@Component
 public class AnsmerMapper {
+
+    public Function<Answer, AnswerDTO> fromAnswerDTO(){
+        return entity ->
+                new AnswerDTO(
+                        entity.getUserId(),
+                        entity.getQuestionId(),
+                        entity.getAnswer()
+                );
+    }
+
+    public Function<AnswerDTO, Answer> fromAnswerDtoToAnswer(String id){
+           return updateAnswer -> {
+               var answer = new Answer();
+               answer.setId(id);
+               answer.setUserId(updateAnswer.getUserId());
+               answer.setQuestionId(updateAnswer.getQuestionId());
+               answer.setAnswer(updateAnswer.getAnswer());
+               answer.setPosition(updateAnswer.getPosition());
+               return answer;
+           };
+    }
 }
